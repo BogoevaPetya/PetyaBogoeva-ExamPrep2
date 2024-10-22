25139a485eae2351211af18918d2a0a591142492
petincka <petincka@abv.bg>
2024-10-22 21:52:31 +0300

    b

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -4,7 +4,7 @@ test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8090/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
-  expect(text).toBe('About');
+  expect(text).toBe('About Petya');
 });
 
 test('Check about page test', async ({ page }) => {
